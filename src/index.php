6dd551696e114ef7ab124e600059e42f53406e08
p4pentest <welcome.abhijit@gmail.com>
2020-09-21 10:27:34 -0400

    Add files via upload

---
+++ src/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello there</title>
+        <style>
+            body {
+                font-family: "Arial", sans-serif;
+                font-size: larger;
+            }
+
+            .center {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 50%;
+            }
+        </style>
+    </head>
+    <body>
+        <img src="https://tech.osteel.me/images/2020/03/04/hello.gif" alt="Hello there" class="center">
+    </body>
+</html>
